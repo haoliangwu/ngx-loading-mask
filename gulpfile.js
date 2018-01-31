1fe4d9eb3203ecf8c63334e011d37b0718fbbbde
Lyon <wuhaoliang@cloudist.cc>
2018-01-31 15:19:00 +0800

    Update 2018-01-31T07:18:47.180Z

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp')
-const ghPages = require('gulp-gh-pages')
-
-function deploy() {
-  return gulp.src('./dist/**/*')
-    .pipe(ghPages())
-}
-
-gulp.task('deploy', deploy)
