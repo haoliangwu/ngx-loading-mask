2e7608ffd6b26b69443624f8fd8394f1de328e70
Lyon <wuhaoliang@cloudist.cc>
2018-02-12 16:47:33 +0800

    [FIX] fix the issue when use ng build with --prod

--- src/app/loading-mask/loading-mask.module.ts
+++ src/app/loading-mask/loading-mask.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ModuleWithProviders } from '@angular/core'
+import { NgModule, ModuleWithProviders, InjectionToken } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { LoadingSnipComponent } from './loading-snip.component'
 import { LoadingMaskDirective } from './loading-mask.directive'
@@ -7,6 +7,12 @@ import { CONFIG, DEFAULT_CONFIG } from './config'
 import { LoadingMaskService } from './loading-mask.service'
 import { HTTP_INTERCEPTORS } from '@angular/common/http'
 import { LoadingMaskInterceptor } from './loading-mask.interceptor'
+
+export const CUSTOM_CONFIG = new InjectionToken<Config>('ngx.loadingMask.custom.config')
+
+const configFactory = (config) => {
+  return {...DEFAULT_CONFIG, ...config}
+}
 
 @NgModule({
   imports: [
@@ -25,17 +31,18 @@ import { LoadingMaskInterceptor } from './loading-mask.interceptor'
 })
 export class LoadingMaskModule {
   static forRoot(config: Config): ModuleWithProviders {
-    const configFactory = () => {
-      return {...DEFAULT_CONFIG, ...config}
-    }
-
     return {
       ngModule: LoadingMaskModule,
       providers: [
         LoadingMaskService,
         {
+          provide: CUSTOM_CONFIG,
+          useValue: config
+        },
+        {
           provide: CONFIG,
-          useFactory: configFactory
+          useFactory: configFactory,
+          deps: [CUSTOM_CONFIG]
         },
         {
           provide: HTTP_INTERCEPTORS,
